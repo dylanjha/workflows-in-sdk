c266177a83f6f3b967b70f1231ba6cfed2249a75
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2025-12-04 21:33:06 -0800

    put the workflow thing in a workflow function

--- nextjs-app/app/api/do-something-1/route.ts
+++ nextjs-app/app/api/do-something-1/route.ts
@@ -1,37 +1,10 @@
 import { start } from 'workflow/api';
 import { NextResponse } from "next/server";
+import { workflowOne } from "../../workflows/workflow-one";
 
 export async function POST(request: Request) {
   const { input } = await request.json();
-  // const run = await start(workflowOne, [input]);
+  const run = await start(workflowOne, [input]);
 
-  // return NextResponse.json(run);
-  return NextResponse.json({ input });
-}
-
-
-export async function workflowOne(input: string) {
-  'use workflow';
-
-  const outputOne = await stepOne(input);
-  const outputTwo = await stepTwo(outputOne as string);
- 
-
-  return { output: outputTwo };
-}
-
-async function stepOne (input: string) {
-  'use step';
-
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(`one ${input}`), 10);
-  });
-}
-
-async function stepTwo (outputOne: string) {
-  'use step';
-
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(`two: ${outputOne}`), 10);
-  });
+  return NextResponse.json({ run });
 }
