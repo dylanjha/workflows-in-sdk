f222522da7ce20b5bb7f0b888a593739025a3b6b
Dylan Jhaveri <dylanjha@users.noreply.github.com>
2025-12-08 21:16:15 -0800

    Remove custom test server directory configuration

--- sdk-with-workflows/nitro.config.ts
+++ sdk-with-workflows/nitro.config.ts
@@ -1,7 +1,6 @@
 import { defineNitroConfig } from 'nitro/config';
 
 export default defineNitroConfig({
-  serverDir: './test-server',
   modules: ['workflow/nitro'],
   compatibilityDate: '2024-01-01',
 });
